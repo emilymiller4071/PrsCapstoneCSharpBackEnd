6f8349f8507cd813935ac1c22c811fb75f5e62d4
emilymiller4071 <emilyrauch3@gmail.com>
2023-02-18 14:51:06 -0500

    added models

--- PrsBackEndCSharp/Models/Request.cs
+++ PrsBackEndCSharp/Models/Request.cs
@@ -1,6 +1,36 @@
-﻿namespace PrsBackEndCSharp.Models
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PrsBackEndCSharp.Models
 {
     public class Request
     {
+        [Key]
+        public int ID { get; set; }
+
+        [StringLength(80)]
+        public string Description { get; set; }
+
+        [StringLength(80)]
+        public string Justification { get; set; }
+
+        [StringLength(80)]
+        public string? RejectionReason { get; set; }
+
+        [StringLength(20)]
+        public string DeliveryMode { get; set; }
+
+        public DateOnly DateNeeded { get; set; }
+
+        [StringLength(10)]
+        public string Status { get; set; }
+
+        [Column(TypeName = "decimal(11,2)")]
+        public decimal Total { get; set; }
+
+        public int UserID { get; set; }
+
+
     }
 }
---
+++ PrsBackEndCSharp/Models/RequestLine.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PrsBackEndCSharp.Models
+{
+    public class RequestLine
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int RequestID { get; set; }
+
+        public int ProductID { get; set; }
+
+        public int Quantity { get; set; }
+
+
+    }
+}
