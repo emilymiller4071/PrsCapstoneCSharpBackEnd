355786a729f4537d5060e5da55f3ba3c4e40ae40
emilymiller4071 <emilyrauch3@gmail.com>
2023-02-18 15:45:28 -0500

    added connection string for sql database (PRS)

---
+++ PrsBackEndCSharp/Models/PrsContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PrsBackEndCSharp.Models
+{
+    public class PrsContext : DbContext   // not a POCO
+    {
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Vendor> Vendors { get; set; }
+        public DbSet<Request> Requests { get; set; }
+        public DbSet<RequestLine> RequestLines { get; set; }
+
+
+        // constructor to support dependency injection (via a service)
+        public PrsContext( DbContextOptions<PrsContext> options ) : base( options )
+        {
+        }
+
+
+    }
+}
--- PrsBackEndCSharp/Program.cs
+++ PrsBackEndCSharp/Program.cs
@@ -1,10 +1,19 @@
 
+using Microsoft.EntityFrameworkCore;
+using PrsBackEndCSharp.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+builder.Services.AddDbContext<PrsContext>(
+    // lambda
+    options => options.UseSqlServer(builder.Configuration.GetConnectionString("PrsConnectionString"))
+    );
+
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
