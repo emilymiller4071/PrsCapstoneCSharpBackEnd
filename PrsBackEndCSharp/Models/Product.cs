6866d47673a9bef9b6870b54ae3dbd4d4f735e3f
emilymiller4071 <emilyrauch3@gmail.com>
2023-03-11 11:30:46 -0500

    data shaping

--- PrsBackEndCSharp/Models/Product.cs
+++ PrsBackEndCSharp/Models/Product.cs
@@ -10,25 +10,31 @@ namespace PrsBackEndCSharp.Models
         [Key] 
         public int ID { get; set; }
 
+        [JsonPropertyName("partNbr")]
         [StringLength(30)]
         [Required]
         public string PartNbr { get; set; }
 
+        [JsonPropertyName("name")]
         [StringLength(30)]
         [Required]
         public string Name { get; set; }
 
+        [JsonPropertyName("price")]
         [Required]
         [Column(TypeName = "decimal(11,2)")]
         public decimal Price { get; set; }
 
+        [JsonPropertyName("unit")]
         [StringLength(30)]
         [Required]
         public string Unit { get; set; }
 
+        [JsonPropertyName("photoPath")]
         [StringLength(255)]
         public string? PhotoPath { get; set; }
 
+        [JsonPropertyName("vendorId")]
         [Required]
         public int VendorID { get; set; }
 
